45bee47c74b6bc705134998707ae8e18c5f3d6d8
gimenavargas <1471366@senati.pe>
2023-03-17 11:09:51 -0500

    suma de valores .inicio

---
+++ bucle_suma/src/bucle_suma/Bucle_suma.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bucle_suma;
+import java.util.Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class Bucle_suma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        Scanner datos=new Scanner(System.in);
+        System.out.println("ingresa tu nombre:");
+        nombre = datos.nextLine();
+        System.out.println("¡Hola " + nombre + "!");
+    }
+    
+    
+}
